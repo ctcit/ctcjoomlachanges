cc2062cb7097efac0101156988c391d74c61e449
Richard Lobb <richard.lobb@canterbury.ac.nz>
2017-06-17 11:01:57 +1200

    Prevent empty forms being emailed to new members rep.

--- plugins/content/ProspectiveMember/ProspectiveMember.php
+++ plugins/content/ProspectiveMember/ProspectiveMember.php
@@ -15,15 +15,20 @@ class plgContentProspectiveMember extends JPlugin
     * <code>{prospectivememberresponse}</code>
     */
     public function onContentPrepare($context, &$row, $params, $page = 0){
-        if ( strpos( $row->text, '{prospectivememberresponse}' ) === false )  // Quick check
+        if ( strpos( $row->text, '{prospectivememberresponse}' ) === false ) { // Quick check
             return true;
-        if ($_POST['email'] == $_POST['email2']) {
-           $row->text = "<p>Your form has been submitted.</p>".
-                        "<p>Thank you for your interest in the CTC.</p>";
+        }
+        if (empty($_POST['email']) || empty($_POST['email2'])) {
+            $row->text = "<p>An email address is required.</p>".
+                         "<p>Please back up in the browser, correct the error and resubmit.</p>";
+            exit;
+        } else if ($_POST['email'] != $_POST['email2']) {
+            $row->text = "<p>Sorry, your form could not be processed as the two email fields do not match.</p>".
+                         "<p>Please back up in the browser, correct the error and resubmit.</p>";
+            exit;
         } else {
-           $row->text = "<p>Sorry, your form could not be processed as the two email fields do not match.</p>".
-                        "<p>Please back up in the browser, correct the error and resubmit.</p>";
-           exit;
+            $row->text = "<p>Your form has been submitted.</p>".
+                         "<p>Thank you for your interest in the CTC.</p>";
         }
         // Now send the email
         jimport( 'joomla.mail.helper' );
@@ -36,7 +41,7 @@ class plgContentProspectiveMember extends JPlugin
         $address = $_POST['address'];
         $postcode = $_POST['postcode'];
         $notes = $_POST['notes'];
-        
+
         $body = "name\t$name\nemail\t$email\nemail2\t$email2\nphone\t$phone\nmobile\t$mobile\naddress\t$address\npostcode\t$postcode\nnotes\t$notes";
         $user = JFactory::getUser();
         $config = JFactory::getConfig();
