b8f3afcf41fed0651136e6ea795294b58b9e0625
DougForster <git@forster.net.nz>
2017-06-26 08:03:01 +1200

    Implement basic file management facility

--- plugins/content/CTCDocs/CTCDocs.php
+++ plugins/content/CTCDocs/CTCDocs.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * plugin to generate a set of download links to documents
  * in a directory tree.
@@ -9,18 +10,18 @@
  * non-committee member, the links are restricted to showing just pdf
  * documents. Otherwise all documents are shown.
  */
-
 defined('_JEXEC') or die();
 
 define('ALPHA_SORT', 0);
 define('LAST_MODIFIED_SORT', 1);
 
-class plgContentCTCDocs extends JPlugin{
+class plgContentCTCDocs extends JPlugin {
 
     static $basePath;
     static $liveSite;
+    static $editallowed = false;
 
-    public function makeDocumentIndex($root, $subdir, $sort=ALPHA_SORT) {
+    public function makeDocumentIndex($root, $subdir, $sort = ALPHA_SORT) {
         // Makes a list of links to all documents in the given subdirectory
         // of the given root directory. The subdirectory name is used as a level
         // 2 header. Files within the given directory are assumed to have
@@ -31,32 +32,50 @@ class plgContentCTCDocs extends JPlugin{
         // and is enclosed in parentheses.
         // Sort order is currently experimental; only call to this uses
         // ALPHA_SORT (the default).
-        $dirname = plgContentCTCDocs::$basePath.DIRECTORY_SEPARATOR.$root.DIRECTORY_SEPARATOR.$subdir;
+        $dirname = plgContentCTCDocs::$basePath . DIRECTORY_SEPARATOR . $root . DIRECTORY_SEPARATOR . $subdir;
         $dir = opendir($dirname);
-        $result = "<h2>$subdir</h2><ul>";
+        $result = '<h2>'; //.
+
+        if (plgContentCTCDocs::$editallowed) {
+            $result .= '<button title = "Upload document" onclick="UploadCTCDocuments(\'' . $root . '\',\'' . $subdir . '\')">+</button>' .
+                    ' <progress class="progress' . $subdir . '" style="display:none"></progress> ';
+        }
+        $result .= $subdir; //.';//</h2><ul>';
+        if (plgContentCTCDocs::$editallowed) {
+            $result .= '  <button title = "Rename folder" onclick="RenameDocumentFolder(\'' . $root . '\',\'' . $subdir . '\')">...</button>';
+        }
+        $result .= '</h2><ul>';
         $lines = array();
         while (($file = readdir($dir)) !== false) {
-            if ($file[0] == '.') continue;
-            $ext = substr($file,-3,3);
-            if ($ext != "doc" && $ext != "pdf") continue;
-                $path = $dirname.DIRECTORY_SEPARATOR.$file;
-                $status = stat($path);
-                $time = $status['mtime'];
-                $size = round($status['size']/1000);
-                $fileBits = explode(".",$file);
-                $descr = $fileBits[0].".$ext";
-                $extra = "";
-                if (count($fileBits) > 2) {
-                    $extra .= " ($fileBits[1])";
-                }
-                $extra .= " -- $size kB";
-                if ($sort === ALPHA_SORT) {
-                    $key = $fileBits[0];
-                } else {
-                    $key = $time;
-                }
-                $lines[$key] = "<li><a href=\"".plgContentCTCDocs::$liveSite."/$root/$subdir/$file\" target=\"_blank\">$descr</a>$extra</li>";
+            if ($file[0] == '.')
+                continue;
+            $ext = substr($file, -3, 3);
+            if ($ext != "doc" && $ext != "pdf")
+                continue;
+            $path = $dirname . DIRECTORY_SEPARATOR . $file;
+            $status = stat($path);
+            $time = $status['mtime'];
+            $size = round($status['size'] / 1000);
+            $fileBits = explode(".", $file);
+            $descr = $fileBits[0] . ".$ext";
+            $extra = "";
+            if (count($fileBits) > 2) {
+                $extra .= " ($fileBits[1])";
             }
+            $extra .= " -- $size kB";
+            if ($sort === ALPHA_SORT) {
+                $key = $fileBits[0];
+            } else {
+                $key = $time;
+            }
+            $line = "<li><a href=\"" . plgContentCTCDocs::$liveSite . "/$root/$subdir/$file\" target=\"_blank\">$descr</a>$extra";
+            if (plgContentCTCDocs::$editallowed) {
+                $line .= '<button style="width:20px;height:20px;margin:1px 0px 1px 5px; padding: 0px 2px 7px 2px;" title = "Remove file"  onclick="RemoveFile(\'' . $root . '\',\'' . $subdir . '\',\'' . $file . '\')">-</button>';
+                $line .= '<button style="width:20px;height:20px;margin:1px 0px 1px 5px; padding: 0px 2px 7px 2px;" title = "Rename file" " onclick="RenameFile(\'' . $root . '\',\'' . $subdir . '\',\'' . $file . '\')">...</button>';
+            }
+            $line .= "</li>";
+            $lines[$key] = $line;
+        }
         ksort($lines);
         foreach ($lines as $line) {
             $result .= $line;
@@ -65,16 +84,22 @@ class plgContentCTCDocs extends JPlugin{
         return $result;
     }
 
-    static public function processDirectory ( $match ) {
+    static public function processDirectory($match) {
         // Called to process a matching {mostripimage directory} insertion
         //global $mosConfig_absolute_path;
+        $user = JFactory::getUser();
+        plgContentCTCDocs::$editallowed = $user->authorise('core.edit');
         if (count($match) != 2) {
             return "";
-        }
-        else {
+        } else {
             $param = $match[1];
-            $output = "";
-            $dirname = plgContentCTCDocs::$basePath.DIRECTORY_SEPARATOR.$param;
+            $output = '<script src="media/jui/js/jquery.min.js" type="text/javascript"></script>' .
+                    '<script src="plugins/content/CTCDocs/ManageDocuments.js"></script>' .
+                    '<DIALOG class = "InputDialog"><input class = "InputDialogText"></input><button class = "InputDialogOK">OK</button></DIALOG>';
+            if (plgContentCTCDocs::$editallowed)
+                $output .= '<button title = "New folder" onclick="NewDocumentFolder(\'' . $param . '\')">+ Folder</button>';
+
+            $dirname = plgContentCTCDocs::$basePath . DIRECTORY_SEPARATOR . $param;
             if (!file_exists($dirname) || !is_dir($dirname)) {
                 return "**ERROR: MISSING DIRECTORY** $dirname";  // No such directory
             }
@@ -82,7 +107,7 @@ class plgContentCTCDocs extends JPlugin{
             $subdirs = array();  // List of subdirectories
             $are_dates = True;  // True if subdirectories look like dates
             while (($subdir = readdir($dir)) !== false) {
-                if ($subdir[0] == '.' || !is_dir($dirname.DIRECTORY_SEPARATOR.$subdir)) {
+                if ($subdir[0] == '.' || !is_dir($dirname . DIRECTORY_SEPARATOR . $subdir)) {
                     continue;
                 }
                 $subdirs[] = $subdir;
@@ -104,21 +129,87 @@ class plgContentCTCDocs extends JPlugin{
         return $output;
     }
 
-
-    public function onContentPrepare($context, &$row, $params, $page = 0){
-        if ( strpos( $row->text, '{mosctcdocs' ) === false ) {  // Quick check
+    public function onContentPrepare($context, &$row, $params, $page = 0) {
+        if (strpos($row->text, '{mosctcdocs') === false) {  // Quick check
             return true;
         }
         $config = JFactory::getConfig();
         plgContentCTCDocs::$basePath = JPATH_BASE;
         plgContentCTCDocs::$liveSite = $config->get("live_site");
-         // define the regular expression for the bot
+        // define the regular expression for the bot
         $regex = "#{mosctcdocs +(.*?)}#";
         // Do all the individual replacements in this document
         // (can handle multiple directories, though that's not a likely use).
-        $row->text = preg_replace_callback( $regex, function($match){return plgContentCTCDocs::processDirectory($match);}, $row->text );
+        $row->text = preg_replace_callback($regex, function($match) {
+            return plgContentCTCDocs::processDirectory($match);
+        }, $row->text);
         return true;
     }
+
+    public function onAjaxManagectcdocuments() {
+        $user = JFactory::getUser();
+        if (!$user->authorise('core.edit'))
+            die("Unauthorised access");
+        $data = array('success' => false, 'message' => 'Operation failed');
+        if (isset($_POST['action']) && isset($_POST['root']) && is_dir($_POST['root'])) {
+            // Assume Ajax call intended for attention here
+            ob_start(); // Buffer any unexpected output
+            $action = $_POST['action'];
+            $root = $_POST['root'];
+            $subdir = $_POST['subdir'];
+            $error = false;
+            if ($action === 'UploadFile' && isset($subdir)) {
+                $uploaddir = JPATH_BASE . DIRECTORY_SEPARATOR . $root . DIRECTORY_SEPARATOR . $subdir;
+                $files = array();
+                $currentfile = '';
+                foreach ($_FILES as $file) {
+                    $currentfile = $file['name'];
+                    if (move_uploaded_file($file['tmp_name'], $uploaddir . DIRECTORY_SEPARATOR . basename($file['name']))) {
+                        $files[] = $uploaddir . $file['name'];
+                    } else {
+                        $error = true;
+                        break;
+                    }
+                }
+                $data = ($error) ? array('success' => false, 'message' => 'Error uploading file ' + $currentfile)
+                                 : array('success' => true, 'message' => $currentfile . ' uploaded');
+            } else if ($action === 'RemoveFile' && isset($subdir) && isset($_POST['filename'])) {
+                $filename = $_POST['filename'];
+                $newfilename = JPATH_BASE . DIRECTORY_SEPARATOR . $root . DIRECTORY_SEPARATOR . $filename;
+                $oldfilename = JPATH_BASE . DIRECTORY_SEPARATOR . $root . DIRECTORY_SEPARATOR . $subdir . DIRECTORY_SEPARATOR . $filename;
+                $error = !rename($oldfilename, $newfilename);
+                $data = ($error) ? array('success' => false, 'message' => 'Error attempting to remove file ' . $filename)
+                                 : array('success' => true, 'message' => $filename . ' removed');
+            } else if ($action === 'RenameFile' && isset($subdir)) {
+                if (isset($_POST['newname']) && isset($_POST['filename']) && strpbrk($_POST['newname'], "\\/?%*:|\"<>") === FALSE) {
+                    $newfilepath = JPATH_BASE . DIRECTORY_SEPARATOR . $root . DIRECTORY_SEPARATOR . $subdir . DIRECTORY_SEPARATOR . $_POST['newname'];
+                    $oldfilepath = JPATH_BASE . DIRECTORY_SEPARATOR . $root . DIRECTORY_SEPARATOR . $subdir . DIRECTORY_SEPARATOR . $_POST['filename'];
+                    $error = file_exists($newfilepath) || !rename($oldfilepath, $newfilepath);
+                    $data = ($error) ? array('success' => false, 'message' => 'Error attempting to rename file ' . $_POST['filename'])
+                                     : array('success' => true, 'message' => $oldfilename . ' renamed to ' . $_POST['newname']);
+                }
+            } else if ($action === 'RenameFolder' && isset($subdir)) {
+                if (isset($_POST['newname']) && strpbrk($_POST['newname'], "\\/?%*:|\"<>") === FALSE) {
+                    $uploaddir = JPATH_BASE . DIRECTORY_SEPARATOR . $root . DIRECTORY_SEPARATOR . $subdir;
+                    $newfoldername = JPATH_BASE . DIRECTORY_SEPARATOR . $root . DIRECTORY_SEPARATOR . $_POST['newname'];
+                    $error = is_dir($newfoldername) || !rename($uploaddir, $newfoldername);
+                    $data = ($error) ? array('success' => false, 'message' => 'Error attempting to rename folder ' . $subdir)
+                                     : array('success' => true, 'message' => $subdir . ' renamed to ' . $_POST['newname']);
+                }
+            } else if ($action === 'NewFolder') {
+                if (isset($_POST['newname']) && strpbrk($_POST['newname'], "\\/?%*:|\"<>") === FALSE) {
+                    $newfoldername = JPATH_BASE . DIRECTORY_SEPARATOR . $root . DIRECTORY_SEPARATOR . $_POST['newname'];
+                    $error = is_dir($newfoldername) || !mkdir($newfoldername);
+                    $data = ($error) ? array('success' => false, 'message' => 'Error attempting to create folder ' . $newfoldername)
+                                     : array('success' => true, 'message' => $newfoldername . ' created');
+                }
+            }
+            ob_end_clean(); // Discard any potential output generated internally by php or other ajax handlers
+            return json_encode($data);
+        }
+        return "";       
+    }
+
 }
 
 ?>
---
+++ plugins/content/CTCDocs/ManageDocuments.js
@@ -0,0 +1,230 @@
+function RemoveFile(p_root, p_subdir, p_filename) {
+    if (confirm('Do you want to remove file ' + p_filename + ' from ' + p_subdir)) {
+        var formdata = new FormData();
+        formdata.append('action', 'RemoveFile');
+        formdata.append('root', p_root);
+        formdata.append('subdir', p_subdir);
+        formdata.append('filename', p_filename);
+        jQuery(function ($) {
+            $.ajax({
+                url: 'index.php?option=com_ajax&plugin=managectcdocuments&group=content&format=raw',
+                type: 'POST',
+                data: formdata,
+                cache: false,
+                contentType: false,
+                processData: false,
+                success: function (data) {
+                    result = JSON.parse(data);
+                    if (result.success) {
+                        //alert(result.message);
+                        location.reload(true); // TODO change to dom if can be bothered
+                    } else
+                        alert(result.message);
+                },
+                error: function (data) {
+                    alert("File removal failed");
+                }
+            });
+        });
+    }
+}
+
+function DoRenameDocumentFolder(p_root, p_subdir) {
+    // code goes here
+    var newname = document.getElementsByClassName("InputDialogText")[0].value;
+    document.getElementsByClassName("InputDialog")[0].close();
+    if (p_subdir !== newname) {
+        var formdata = new FormData();
+        formdata.append('action', 'RenameFolder');
+        formdata.append('root', p_root);
+        formdata.append('subdir', p_subdir);
+        formdata.append('newname', newname);
+        jQuery(function ($) {
+            $.ajax({
+                url: 'index.php?option=com_ajax&plugin=managectcdocuments&group=content&format=raw',
+                type: 'POST',
+                data: formdata,
+                cache: false,
+                contentType: false,
+                processData: false,
+                success: function (data) {
+                    result = JSON.parse(data);
+                    if (result.success){
+                        //alert(result.message);
+                        location.reload(true); // TODO change to dom if can be bothered
+                    }else
+                        alert(result.message);
+                },
+                error: function (data) {
+                    alert("Folder rename failed");
+                }
+            });
+        });
+    }
+}
+
+function RenameDocumentFolder(p_root, p_subdir){
+    dialog = document.getElementsByClassName("InputDialog")[0];
+    inputtext = document.getElementsByClassName("InputDialogText")[0];
+    inputtext.value = p_subdir;
+    okbutton = document.getElementsByClassName("InputDialogOK")[0];
+    okbutton.addEventListener('click', DoRenameDocumentFolder.bind(null, p_root, p_subdir), false);
+    dialog.showModal();
+}
+
+function DoRenameFile(p_root, p_subdir, p_filename) {
+    dialog = document.getElementsByClassName("InputDialog")[0];
+    var newname = document.getElementsByClassName("InputDialogText")[0].value;
+    dialog.close();
+    if (p_filename !== newname) {
+        var formdata = new FormData();
+        formdata.append('action', 'RenameFile');
+        formdata.append('root', p_root);
+        formdata.append('subdir', p_subdir);
+        formdata.append('filename', p_filename);
+        formdata.append('newname', newname);
+        jQuery(function ($) {
+            $.ajax({
+                url: 'index.php?option=com_ajax&plugin=managectcdocuments&group=content&format=raw',
+                type: 'POST',
+                data: formdata,
+                cache: false,
+                contentType: false,
+                processData: false,
+                success: function (data) {
+                    result = JSON.parse(data);
+                    if (result.success){
+                        //alert(result.message);
+                        location.reload(true); // TODO change to dom if can be bothered
+                    }else
+                        alert(result.message);
+                 },
+                error: function (data) {
+                    alert("File rename failed");
+                }
+            });
+        });
+    }
+}
+
+function RenameFile(p_root, p_subdir, p_filename){
+    dialog = document.getElementsByClassName("InputDialog")[0];
+    inputtext = document.getElementsByClassName("InputDialogText")[0];
+    inputtext.value = p_filename;
+    okbutton = document.getElementsByClassName("InputDialogOK")[0];
+    okbutton.addEventListener('click', DoRenameFile.bind(null, p_root, p_subdir, p_filename), false);
+    dialog.showModal();
+}
+
+function UploadCTCDocuments(p_root, p_subdir) {
+    //choosefile doesn't need to be visible or added to document
+    var choosefile = document.createElement('input');
+    choosefile.type = 'file';
+    choosefile.class = 'inputFile';
+    choosefile.title = "Choose new document";
+    choosefile.addEventListener('change', DoUpload.bind(null, p_root, p_subdir, choosefile), false);
+    choosefile.click();
+}
+
+function DoUpload(p_root, p_subdir, p_choosefile) {
+    var files = p_choosefile.files;
+    if (files.size === 0)
+        return;
+    var formdata = new FormData();
+    formdata.append(files[0].name, files[0]);
+    formdata.append('action', 'UploadFile');
+    formdata.append('root', p_root);
+    formdata.append('subdir', p_subdir);
+    var progress = document.getElementsByClassName('progress' + p_subdir)[0];
+    jQuery(function ($) {
+        $.ajax({
+            // Process upload via Joomla ajax component
+            // Note that there doesn't need to actually be a plugin with that name
+            // Just any plugin with onAjax... method in the specified group
+            // In this case handled by the CTCDocs plugin
+            url: 'index.php?option=com_ajax&plugin=managectcdocuments&group=content&format=raw',
+            type: 'POST',
+            // Form data
+            data: formdata,
+            // Tell jQuery not to process data or worry about content-type
+            // You *must* include these options!
+            cache: false,
+            contentType: false,
+            processData: false,
+            // Custom XMLHttpRequest
+            xhr: function () {
+                var xhrCustom = $.ajaxSettings.xhr();
+                if (xhrCustom.upload) {
+                    // For handling the progress of the upload
+                    xhrCustom.upload.addEventListener('progress',
+                            function (e) {
+                                if (e.lengthComputable) {
+                                    progress.style.display = "inline-block";
+                                    progress.value = e.loaded;
+                                    progress.max = e.total;
+                                }
+                            }
+                    , false);
+                }
+                return xhrCustom;
+            },
+            success: function (data) {
+                progress.style.display = "none";
+                result = JSON.parse(data);
+                if (result.success) {
+                    //alert(result.message);
+                    location.reload(true); // TODO change to dom if can be bothered
+                } else
+                    alert(result.message);
+            },
+            error: function (data) {
+                progress.style.display = "none";
+                alert("Upload failed");
+            }
+        });
+    });
+
+}
+
+function DoNewDocumentFolder(p_root) {
+    // code goes here
+    var newname = document.getElementsByClassName("InputDialogText")[0].value;
+    document.getElementsByClassName("InputDialog")[0].close();
+    var formdata = new FormData();
+    formdata.append('action', 'NewFolder');
+    formdata.append('root', p_root);
+    formdata.append('newname', newname);
+    jQuery(function ($) {
+        $.ajax({
+            url: 'index.php?option=com_ajax&plugin=managectcdocuments&group=content&format=raw',
+            type: 'POST',
+            data: formdata,
+            cache: false,
+            contentType: false,
+            processData: false,
+            success: function (data) {
+                result = JSON.parse(data);
+                if (result.success) {
+                    //alert(result.message);
+                    location.reload(true); // TODO change to dom if can be bothered
+                } else
+                    alert(result.message);
+            },
+            error: function (data) {
+                alert("New folder failed");
+            }
+        });
+    });
+}
+
+function NewDocumentFolder(p_root){
+    dialog = document.getElementsByClassName("InputDialog")[0];
+    inputtext = document.getElementsByClassName("InputDialogText")[0];
+    inputtext.value = '';
+    okbutton = document.getElementsByClassName("InputDialogOK")[0];
+    okbutton.addEventListener('click', DoNewDocumentFolder.bind(null, p_root), false);
+    dialog.showModal();
+}
+;
+
+
