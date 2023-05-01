c0a992e1ecfc9e08adb24331c0cfeb6a01c33a09
Nick Edwards <nickedwrds@gmail.com>
2023-05-01 22:36:05 +1200

    Improve homepage welcome

--- modules/mod_ctc_welcome/tmpl/default.php
+++ modules/mod_ctc_welcome/tmpl/default.php
@@ -15,24 +15,37 @@ use Joomla\CMS\Helper\ModuleHelper;
 if ($isMember) {
   ?>
 <h1 class="home-h1">Welcome <?=$user->name;?></h1>
+  <a href="" class="btn btn-outline-warning home-btn"><i class="fas fa-hiking home-btn-icon"></i><br>Trip Signup</a>
+  <a href="" class="btn btn-outline-warning home-btn"><i class="fas fa-edit home-btn-icon"></i><br>Create Trip Report</a>
+  <a href="" class="btn btn-outline-warning home-btn"><i class="fas fa-calendar home-btn-icon"></i><br>Hut Booking Calendar</a>
+  <a href="" class="btn btn-outline-warning home-btn"><i class="fas fa-user home-btn-icon"></i><br>Your CTC Account</a>
+  <a href="" class="btn btn-outline-warning home-btn"><i class="fas fa-key home-btn-icon"></i><br>Change Password</a>
+  <a href="" class="btn btn-outline-warning home-btn"><i class="fas fa-campground home-btn-icon"></i><br>Gear Hire</a>
+
 <ul>
-  <li><a href="">Trip Signup</a></li>
-  <li><a href="">Edit Member Details</a></li>
-  <li><a href="">Change Password</a></li>
+  <!--<li><a href="">Trip Signup <i class="fas fa-hiking"></i></a></li>
+  <li><a href="">Create Trip Report <i class="fas fa-edit"></i></a></li>
+  <li><a href="">Hut Booking Calendar <i class="fas fa-calendar"></i></a></li>
+  <li><a href="">Your CTC Account <i class="fas fa-user"></i></a></li>
+  <li><a href="">Change Password <i class="fas fa-key"></i></a></li>-->
 </ul>
 <?php
 
 } else {
   ?>
-<h1 class="home-h1">Welcome to the CTC!</h1>
-<p>The Christchurch Tramping Club (CTC) has around 350 members of all ages and runs
-tramping trips every weekend. Our trips range from easy (minimal experience
-required) to hard (high level of fitness and experience required). We also
-organise some instruction courses, have some equipment for hire, and hold
-informal weekly meetings and other social events. We're a friendly and social
+<h1 class="home-h1">Welcome to the CTC</h1>
+<p>The Christchurch Tramping Club (CTC) is one of Christchurch's largest
+tramping clubs. We run a range of day and overnight trips most weekends as well
+as regular socials. Our trips range from easy (minimal experience
+required) to hard (high level of fitness and experience required).
+We also organise instruction courses and have a range of equipment for hire</p>
+<p> We're a friendly and social
 club and always welcome new members, whatever your age, ability or background!</p>
 <div class="clearfix">
-    <a href="index.php/join-us" class="ctc-button btn btn-primary px-5 float-right" role="button">Join Us</a>
+    <div class="float-right">
+      <a href="index.php/about/about-ctc" class="ctc-button-outline btn btn-primary px-5 m-2" role="button">Learn More</a>
+      <a href="index.php/join-us" class="ctc-button btn btn-primary px-5 m-2" role="button">Join Us</a>
+    </div>
 </div>
 
 <?php }
